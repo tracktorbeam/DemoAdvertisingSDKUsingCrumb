c2626c6983537b2e19a2459b1a07975413cc6e99
Arpan Ghosh <arpan@tracktorbeam.com>
2014-02-10 19:38:58 -0800

    removing the constant advertising ID

--- DemoAdvertisingSDKUsingCrumb/DemoAdSDKUsingCrumbNetworkManager.m
+++ DemoAdvertisingSDKUsingCrumb/DemoAdSDKUsingCrumbNetworkManager.m
@@ -36,8 +36,7 @@
 }
 
 - (void)fetchAdvertisement{
-    //[[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString]
-    [self.crumbNetworkManager GET:@"73A7F08B-4591-4A27-AB76-57E054B176B7" parameters:@{} success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    [self.crumbNetworkManager GET:[[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString] parameters:@{} success:^(AFHTTPRequestOperation *operation, id responseObject) {
         if (!responseObject) {
             [[DemoAdvertisingSDKUsingCrumbManager getAdManager] noAdInventoryAvailableOnCrumb];
         }else{
